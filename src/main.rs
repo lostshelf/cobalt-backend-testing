d94831f8fbe798fc640e6ab2d8be32f04d1c8ee8
lostshelf <hadifaraz52@protonmail.com>
2022-06-10 23:06:15 -0600

    added some verbose ass code

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use std::net::{Ipv4Addr};
 use std::fmt::{self, Formatter};
-use chrono::{Date, Utc};
-use clap::{Command, arg};
+use std::str::FromStr;
+use chrono::{DateTime, Utc};
+use clap::{Command, arg, ArgMatches};
 use rand::Rng;
+use pnet::datalink;
 
 struct User {
     name: String,
-    id: usize,
     ip: Ipv4Addr,
 }
 
@@ -14,8 +15,18 @@ impl Default for User {
     fn default() -> Self {
         User {
             name: String::from("host"),
-            id: rand::thread_rng().gen(),
             ip: Ipv4Addr::new(127, 0, 0, 1),
+        }
+    }
+}
+
+impl From<ArgMatches> for User {
+    fn from(m: ArgMatches) -> Self {
+        User {
+            name: m.value_of("name").unwrap_or_else(|| {
+                "host"
+            }).to_string(),
+            ip: Ipv4Addr::new(127, 0, 0, 1), // TODO: Find a way to
         }
     }
 }
@@ -30,17 +41,17 @@ struct Message {
     contents: String,
     id: usize,
     sender: User,
-    date: Date<Utc>,
+    date: DateTime<Utc>
 }
 
 impl fmt::Display for Message {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "<{}> {}: {}", self.date, self.sender.name, self.contents)
+        write!(f, "<{}> {}: {}", self.date.time(), self.sender.name, self.contents)
     }
 }
 
 fn main() {
-    let matches = Command::new("Cobalt Backend Testing")
+    let matches=Command::new("Cobalt Backend Testing")
         .author("Hadi Faraz, <hadifaraz52@protonmail.com>")
         .version("0.1.0")
         .about("Sends a message to another computer with the power of TCP.")
@@ -54,6 +65,14 @@ fn main() {
                 .required(false)
                 .allow_invalid_utf8(true),
         ).get_matches();
+
+    let _user = User::from(matches);
+
+    let _target: Ipv4Addr = Ipv4Addr::from_str(matches.value_of("address").unwrap_or_else(|| {
+        "127.0.0.1"
+    })).unwrap_or_else(|| {
+        Ipv4Addr::new(127, 0, 0, 1)
+    });
 
 
 }
